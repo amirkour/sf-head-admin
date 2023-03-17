18cc77a53babbea1a68134456dbe28c67eae727d
Amir Kouretchian <amir.kouretchian@gmail.com>
2023-03-16 21:20:13 -0700

    more config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xs: "375px",
+      sm: "576px",
+      ...defaultTheme.screens,
+    },
     extend: {},
   },
   plugins: [],
