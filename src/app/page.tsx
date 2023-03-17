37677ac79083052878bbaef7328ce6562b7cfdef
Amir Kouretchian <amir.kouretchian@gmail.com>
2023-03-16 21:03:44 -0700

    adding tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main>hi world</main>;
+  return (
+    <main>
+      <h1 className="text-center">hi world</h1>
+      <p>this is a tailwind paragraph!?</p>
+    </main>
+  );
 }
